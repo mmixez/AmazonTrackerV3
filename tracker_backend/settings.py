6c8894d13c6b8c12be4ede64e8d374d782dd5864
Younki <younki@Younkis-MacBook-Pro.local>
2026-04-02 07:58:42 -0700

    Fix: URL validation to handle missing HTTPS protocol

--- tracker_backend/settings.py
+++ tracker_backend/settings.py
@@ -63,16 +63,28 @@ WSGI_APPLICATION = 'tracker_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "amazontracker_db",  # your database name
+        "USER": "younki",            # the role you just used
+        "PASSWORD": "1111", # your actual password
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
